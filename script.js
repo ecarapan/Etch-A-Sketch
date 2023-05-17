75af7e6973a5c464effde93544e01f02cca9f56d
ecarapan <92954426+ecarapan@users.noreply.github.com>
2023-05-17 13:23:39 -0700

    Implement hover affect for each grid square

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ for (let i = 0; i < 16; i++) {
     grid.appendChild(row);
 }
 
+const squares = Array.from(document.querySelectorAll('.square'));
+squares.forEach(square => square.addEventListener('mouseenter', handleMouseEnter));
+function handleMouseEnter(e) {
+    e.target.style.backgroundColor = 'rgb(173, 216, 230)';
+}
+
+
+
+
 
 
